d6189fb8e45e34e384c16a7f5d3fb05b8b6f8ce1
Sarkar-Soumyadeep-202002 <soumyadeep.sarkar@my.jcu.edu.au>
2022-03-30 10:50:50 +0530

    Add Password Checker program

---
+++ password_check.py
@@ -0,0 +1,23 @@
+"""
+Name: Soumyadeep Sarkar
+Description: Password checker
+
+"""
+
+
+def is_valid(password,length):
+    if len(password)<length:
+        return False
+    else:
+        return True
+
+
+def main():
+    password=input("Enter your password: ")
+    length=10
+    print(f"Your password is valid? {is_valid(password,length)}")
+    for char in range(0,len(password)):
+        print('*',end='')
+
+
+main()
