bfb5a6ccff4d1dbcd9f39286fe2c611a9c0e979a
Sarkar-Soumyadeep-202002 <soumyadeep.sarkar@my.jcu.edu.au>
2022-03-30 19:33:29 +0530

    Improve indentation and spacing in the Password Checker program

--- password_check.py
+++ password_check.py
@@ -5,19 +5,19 @@ Description: Password checker
 """
 
 
-def is_valid(password,length):
-    if len(password)<length:
+def is_valid(password, length):
+    if len(password) < length:
         return False
     else:
         return True
 
 
 def main():
-    password=input("Enter your password: ")
-    length=10
+    password = input("Enter your password: ")
+    length = 10
     print(f"Your password is valid? {is_valid(password,length)}")
-    for char in range(0,len(password)):
-        print('*',end='')
+    for char in range(0, len(password)):
+        print('*', end='')
 
 
 main()
